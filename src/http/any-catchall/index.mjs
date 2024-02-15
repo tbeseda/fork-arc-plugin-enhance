104ad98189b3e8ed95a5bb93d7448e33d4860f7a
Taylor Beseda <tbeseda@gmail.com>
2024-02-14 22:17:20 -0700

    mount core with loader in enhance actual

--- src/http/any-catchall/index.mjs
+++ src/http/any-catchall/index.mjs
@@ -1,14 +1,84 @@
-import path from 'path'
-import url from 'url'
-import arc from '@architect/functions'
-import router from './router.mjs'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-export function createRouter (base) {
-  if (!base) {
-    let here = path.dirname(url.fileURLToPath(import.meta.url))
-    base = path.join(here, 'node_modules', '@architect', 'views')
-  }
-  return arc.http(router.bind({}, { basePath: base }))
+import arc from '@architect/functions'
+import importTransform from '@enhance/import-transform'
+import styleTransform from '@enhance/enhance-style-transform'
+import loadAppConfig from './app-loader/src/index.js'
+import createEnhanceApp from './app-core/src/index.js'
+
+import defaultHead from './templates/head.mjs'
+
+const here = dirname(fileURLToPath(import.meta.url))
+const basePath = join(here, 'node_modules', '@architect', 'views')
+
+let head
+try {
+  const headModule = await import(join(basePath, 'head.mjs'))
+  head = headModule.default
+}
+catch (err) {
+  head = defaultHead
 }
 
-export const handler = createRouter()
+let preflight
+try {
+  const preflightModule = await import(join(basePath, 'preflight.mjs'))
+  preflight = preflightModule.default
+}
+catch (err) {
+  preflight = () => ({})
+}
+
+const config = await loadAppConfig({ basePath })
+
+const app = createEnhanceApp({
+  ...config,
+  head,
+  ssrOptions: {
+    scriptTransforms: [ importTransform({ lookup: arc.static }) ],
+    styleTransforms: [ styleTransform ],
+  },
+})
+
+async function http (req) {
+  try {
+    const moreState = await preflight(req)
+    const response = await app.routeAndRender(req, moreState)
+
+    // merge timing headers
+    const headers = {
+      ...response.headers,
+      [config.timers.key]: [
+        config.timers.value(),
+        response.headers?.[config.timers.key],
+      ].join(', '),
+    }
+
+    return { ...response, headers }
+  }
+  catch (err) {
+    const status = Number(err.message) // Core throws status code 404 or 500
+    if (status === 404) {
+      // render 404 page from templates
+    }
+    else if (status === 500) {
+      // render 500 page from templates
+    }
+    // generic error page
+    return {
+      statusCode: 500,
+      html: await app.render(`
+          <main>
+            <h1>${err.message}</h1>
+            <h2>${err.cause}</h2>
+            <p>
+              <pre>${err.stack}</pre>
+            </p>
+          </main>
+        `, { error: err }),
+    }
+  }
+}
+
+export const handler = arc.http(http)
